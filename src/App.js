e61a4419b8471858edeed7761c54132015a47869
Alura <aluracm@MacBook-Pro.local>
2021-05-25 14:53:08 -0500

    Added ResponseDisplay component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import ResponseDisplay from './ResponseDisplay'
 
 function App() {
   return (
-    <h1 className="App">
-      PeachQL React App
-    </h1>
+    <div id="App">
+      <h1 className="App">PeachQL React App</h1>
+      <div id="ResponseDisplay">
+      <ResponseDisplay />
+      </div>
+    </div>  
   );
 }
 
